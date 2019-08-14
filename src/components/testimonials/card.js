0380e5d2b7340fa589fca79073ba8af4f177dd96
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-08-14 14:51:06 +0300

    add tests for testimonial components

--- src/components/testimonials/card.js
+++ src/components/testimonials/card.js
@@ -4,13 +4,13 @@ import './card.scss';
 const Card = ({name, image, company, color, testimonial}) => (
     <div className={`card ${color}`}>
         <div className='upperSection'>
-            <h2>{name}</h2>
-            <p>{testimonial}</p>
+            <h2 className='h2-card-upper'>{name}</h2>
+            <p className='paragraphly'>{testimonial}</p>
             <img src={image}/>
         </div>
         <div className='lowerSection'>
-            <h2>Now AT</h2>
-            <h3>{company}</h3>
+            <h2 className='h2-card-lower'>Now AT</h2>
+            <h3 className='h3-card-lower'>{company}</h3>
         </div>
     </div>
 )
--- src/components/testimonials/layout.js
+++ src/components/testimonials/layout.js
@@ -13,21 +13,21 @@ class Testimonials extends React.Component {
                     image={profile} 
                     name={"Mike"} 
                     company="Google" 
-                    color="one"
+                    color="green-border-card"
                     testimonial="Ultimately, CodeImpact is what really became the driving force behind the career change"
                     />
                 <Card 
                     image={patrick} 
                     name={"Patrick"} 
                     company="Facebook" 
-                    color="two"
+                    color="blue-border-card"
                     testimonial="Before CodeImpact, I was working two minimum wage jobs. Now, I have a new job, a new mindset, and new life!"
                     />
                 <Card 
                     image={alexis} 
                     name={"Alexis"} 
                     company="IBM" 
-                    color="three"
+                    color="purple-border-card"
                     testimonial="While freelancing as an interior designer, I took the Intro to programing to learn a new set of skills."
                     />
             </div>
