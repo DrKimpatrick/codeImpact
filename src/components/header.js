030a67585321a42aa915f73220b01234a624c2e1
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-09-14 17:02:01 +0300

    add effects to header

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,29 @@ import logo from '../images/logo3.jpg'
 import Humburger from './hamburger'
 
 class Header extends React.Component {
+  state = {
+    hasScroll: false
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll);
+
+  }
+
+  handleScroll = () => {
+    // Getting the number of pixels the document has already been scrolled from the top
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 520){
+      this.setState({hasScroll: true});
+    }else{
+      this.setState({hasScroll: false});
+    }
+  }
+
   render(){
     return(
-      <div className='Header'>
+      <div className={this.state.hasScroll ? 'Header HeaderHasScroll': 'Header'}>
           <div id='humburgerIcon'><Humburger/></div>
           <div id='imageWrapper'><img src={logo} id='logo' alt='company logo'/></div>
           <div className='HeaderGroup'>
