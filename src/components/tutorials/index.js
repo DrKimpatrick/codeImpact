46e3ea57c5d242906d70e4a99c66dc82d7d1ed78
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-10-19 21:23:12 +0300

    middle and lower section flexible

--- src/components/tutorials/index.js
+++ src/components/tutorials/index.js
@@ -1,46 +1,87 @@
 import React from 'react';
 import './index.scss';
 
+const TutPeriodOne = () => (
+    <div class="row text-center text-lg-left">
+        <div class="col-lg-3 col-md-4 col-6">
+            <span class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/G9Rfc1qccH4/400x300" alt=""/>
+            </span>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+            <span class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/aJeH0KcFkuc/400x300" alt=""/>
+            </span>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+            <span class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt=""/>
+            </span>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+            <span class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt=""/>
+            </span>
+        </div>
+    </div>
+)
+
+const TutPeriodTwo = () => (
+    <div class="row text-center text-lg-left">
+        <div class="col-lg-3 col-md-4 col-6">
+            <span class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/G9Rfc1qccH4/400x300" alt=""/>
+            </span>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+            <span class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/aJeH0KcFkuc/400x300" alt=""/>
+            </span>
+        </div>
+    </div>
+)
+
 class TutorialSection extends React.Component {
+    state = {
+        tabId: 'today'
+    }
+
+    tabClick = (tab) => {
+        this.setState({tabId: tab.target.id})
+    }
     render(){
         return(
             <div className='container'>
-                <div className='row'>
-                    <span>One</span>
+                <div className='row text-center text-lg-left top-section d-flex align-items-center'>
+                    <div className='col-lg-6 col-md-6 col-12 tut-wording'><span id='top-tut-header'>Getting started with JavaScript</span><br/><span id='top-tut-p'>Learn and master JavaScript programming in 10 days</span></div>
+                    {/* <div className='col-lg-6 col-md-6 col-12 '><button>SEE OUR PROGRAMS</button></div> */}
                 </div>
                 <div className='row text-center text-lg-left'>
-                    <div className='col-lg-6 col-md-4 col-12'>Trending Tutorials</div>
+                    <div className='col-lg-6 col-md-4 col-12 period align-middle' id='middle-header'>Trending Tutorials</div>
                     <div className='col-lg-6 col-md-8 col-12'>
                         <div className='row text-center text-lg-left'>
-                            <div className='col-lg-3 col-md-3 col-3'>Today</div>
-                            <div className='col-lg-3 col-md-3 col-3'>Week</div>
-                            <div className='col-lg-3 col-md-3 col-3'>Month</div>
-                            <div className='col-lg-3 col-md-3 col-3'>Year</div>
+                            <div className='col-lg-3 col-md-3 col-3 period align-middle'><span className={this.state.tabId == 'today'? 'period-in active': 'period-in'} id='today' onClick={this.tabClick}>Today</span></div>
+                            <div className='col-lg-3 col-md-3 col-3 period align-middle'><span className={this.state.tabId == 'week'? 'period-in active': 'period-in'} id='week' onClick={this.tabClick}>Week</span></div>
+                            <div className='col-lg-3 col-md-3 col-3 period align-middle'><span className={this.state.tabId == 'month'? 'period-in active': 'period-in'} id='month' onClick={this.tabClick}>Month</span></div>
+                            <div className='col-lg-3 col-md-3 col-3 period align-middle'><span className={this.state.tabId == 'year'? 'period-in active': 'period-in'} id='year' onClick={this.tabClick}>Year</span></div>
                         </div>
                     </div>
                 </div>
-                <div class="row text-center text-lg-left">
-                    <div class="col-lg-3 col-md-4 col-6">
-                        <span class="d-block mb-4 h-100">
-                            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/G9Rfc1qccH4/400x300" alt=""/>
-                        </span>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-6">
-                        <span class="d-block mb-4 h-100">
-                            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/aJeH0KcFkuc/400x300" alt=""/>
-                        </span>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-6">
-                        <span class="d-block mb-4 h-100">
-                            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt=""/>
-                        </span>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-6">
-                        <span class="d-block mb-4 h-100">
-                            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt=""/>
-                        </span>
-                    </div>
-                </div>
+                <hr class="mt-2 mb-5"/>
+                {(
+                () => {
+                switch(this.state.tabId) {
+                case 'today':
+                    return <TutPeriodOne  />;
+                case 'week':
+                    return <TutPeriodTwo />;
+                case 'month':
+                    return <TutPeriodOne />;
+                default:
+                    return <TutPeriodTwo/>;
+                }
+            }
+            )()}
             </div>
         )
     }
