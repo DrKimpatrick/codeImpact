657e12904d7eade5d61509a73fc0de391eccbf49
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-10-27 21:54:12 +0300

    add validations on subscriber section

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -7,7 +7,9 @@ import {
 
 class NewsLetter extends React.Component {
     state = {
-        email: ''
+        email: '',
+        isChecked: true,
+        error: ''
     }
     componentDidMount() {
         // Initialize the App Client
@@ -21,26 +23,42 @@ class NewsLetter extends React.Component {
         // Get a reference to the todo database
         this.db = mongodb.db("codeimpact");
       }
-      afterSubscribe = () => {
-        
-      }
+
       addSubscriber = (event) => {
         event.preventDefault();
-        const { email } = this.state;
+        const { email, isChecked } = this.state;
+        if(!email){
+          this.setState({ error: "Please provide your email" });
+          return;
+        }
+        if(!isChecked){
+          this.setState({ error: "Please check the box to agree to the terms & conditions" });
+          return;
+        }
         // insert the subscriber into the remote Stitch DB
         this.db
           .collection("subscribers")
           .insertOne({
             email
           })
-          .then(this.afterSubscribe)
+          .then(this.setState({ email: '', error: '' }))
           .catch(console.error);
-        console.log(this.state.email);
       }
 
       handleChange = (event) => {
         this.setState({ email: event.target.value });
-        console.log(this.state.email)
+        var re = /\S+@\S+\.\S+/;
+        if(!re.test(event.target.value)){
+          this.setState({ error: "Email is invalid" });
+        }else {
+          this.setState({ error: "" });
+        }
+      }
+
+      toggleChange = () => {
+        this.setState({
+          isChecked: !this.state.isChecked,
+        });
       }
 
       render(){
@@ -50,8 +68,9 @@ class NewsLetter extends React.Component {
                     <h3>Stay up to date on our latest news and events.</h3>
                     <form onSubmit={this.addSubscriber} id='subscriber'>
                         <input type='email' placeholder='Enter your email' className='email-input' value={this.state.email} onChange={this.handleChange}/><br/>
-                        <input type='checkbox' className='checkbox-accept'/><span className='accept-parent'>I accept  the <span className='blue-accept'>Privacy Policy</span> and  the <span className='blue-accept'>Terms of use</span></span><br/>
+                        <input type='checkbox' className='checkbox-accept' checked={this.state.isChecked} onChange={this.toggleChange}/><span className='accept-parent'>I accept  the <span className='blue-accept'>Privacy Policy</span> and  the <span className='blue-accept'>Terms of use</span></span><br/>
                     </form>
+                    <span id='error'>{this.state.error}</span>
                 </div>
                 <div className='right-letter-div'>
                     <button type='submit' form="subscriber">Get Updates</button>
