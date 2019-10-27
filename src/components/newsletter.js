6bd4bb4680fe455b0eb1290ca5e3037715c52f8b
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-10-27 21:10:59 +0300

    use mongodb stich a mongodb as a service

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import './newsletter.scss';
+import {
+    Stitch,
+    RemoteMongoClient
+  } from "mongodb-stitch-browser-sdk";
 
-const newsLetter = () => (
-    <div className='letter-wrapper'>
-        <div className='left-letter-div'>
-            <h3>Stay up to date on our latest news and events.</h3>
-            <form>
-                <input type='email' placeholder='Enter your email' className='email-input'/><br/>
-                <input type='checkbox' className='checkbox-accept'/><span className='accept-parent'>I accept  the <span className='blue-accept'>Privacy Policy</span> and  the <span className='blue-accept'>Terms of use</span></span><br/>
-            </form>
-        </div>
-        <div className='right-letter-div'>
-            <button>Get Updates</button>
-        </div>
-    </div>
-)
+class NewsLetter extends React.Component {
+    state = {
+        email: ''
+    }
+    componentDidMount() {
+        // Initialize the App Client
+        this.client = Stitch.initializeDefaultAppClient("codeimpact-efvqy");
+        // Get a MongoDB Service Client
+        // This is used for logging in and communicating with Stitch
+        const mongodb = this.client.getServiceClient(
+          RemoteMongoClient.factory,
+          "mongodb-atlas"
+        );
+        // Get a reference to the todo database
+        this.db = mongodb.db("codeimpact");
+      }
+      afterSubscribe = () => {
+        
+      }
+      addSubscriber = (event) => {
+        event.preventDefault();
+        const { email } = this.state;
+        // insert the subscriber into the remote Stitch DB
+        this.db
+          .collection("subscribers")
+          .insertOne({
+            email
+          })
+          .then(this.afterSubscribe)
+          .catch(console.error);
+        console.log(this.state.email);
+      }
 
-export default newsLetter;
+      handleChange = (event) => {
+        this.setState({ email: event.target.value });
+        console.log(this.state.email)
+      }
+
+      render(){
+        return(
+            <div className='letter-wrapper'>
+                <div className='left-letter-div'>
+                    <h3>Stay up to date on our latest news and events.</h3>
+                    <form onSubmit={this.addSubscriber} id='subscriber'>
+                        <input type='email' placeholder='Enter your email' className='email-input' value={this.state.email} onChange={this.handleChange}/><br/>
+                        <input type='checkbox' className='checkbox-accept'/><span className='accept-parent'>I accept  the <span className='blue-accept'>Privacy Policy</span> and  the <span className='blue-accept'>Terms of use</span></span><br/>
+                    </form>
+                </div>
+                <div className='right-letter-div'>
+                    <button type='submit' form="subscriber">Get Updates</button>
+                </div>
+            </div>
+        )
+      }
+
+}
+
+export default NewsLetter;
