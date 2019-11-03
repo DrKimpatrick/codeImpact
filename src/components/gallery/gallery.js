95ee53ada4ce4eb85e37165091923c0e1c3c0d98
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-11-03 11:27:08 +0300

    pick images from data structure

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './gallery';
+import galleryData from './galleryData';
 
 const Gallery = () => (
     // <!-- Page Content -->
@@ -10,67 +11,17 @@ const Gallery = () => (
     <hr className="mt-2 mb-5"/>
 
     <div className="row text-center text-lg-left">
-
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/aob0ukAYfuI/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/EUfxH-pze7s/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/M185_qYH8vg/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/AvhMzHwiE_0/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/2gYsZUmockw/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/EMSDtjVHdQ8/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/8mUEy0ABdNE/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/G9Rfc1qccH4/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/aJeH0KcFkuc/400x300" alt=""/>
-            </span>
-        </div>
-        <div className="col-lg-3 col-md-4 col-6">
-            <span className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt=""/>
-            </span>
-        </div>
+        {
+            galleryData.map((item, index) => {
+                return (
+                    <div className="col-lg-3 col-md-4 col-6" key={index}>
+                        <span className="d-block mb-4 h-100">
+                            <img className="img-fluid img-thumbnail" src={item.image} alt=""/>
+                        </span>
+                    </div>
+                )
+            })
+        }
     </div>
 
 </div>
