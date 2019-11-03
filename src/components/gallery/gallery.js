27f1f4212bfc710c591cca0ee54761f9db4654e3
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-11-03 14:32:07 +0300

    display zoomed image in modal pop-up

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,30 +1,75 @@
 import React from 'react';
-import './gallery';
+import './gallery.scss';
 import galleryData from './galleryData';
 
-const Gallery = () => (
-    // <!-- Page Content -->
-<div className="container" id='gallery'>
+class Gallery extends React.Component {
 
-    <h1 className="font-weight-light text-center mt-4 mb-0">CodeImpact Moments</h1>
+    componentDidMount(){
+        // Get the modal
+        var modal = document.getElementById("myModal");
 
-    <hr className="mt-2 mb-5"/>
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
 
-    <div className="row text-center text-lg-left">
-        {
-            galleryData.map((item, index) => {
-                return (
-                    <div className="col-lg-3 col-md-4 col-6" key={index}>
-                        <span className="d-block mb-4 h-100">
-                            <img className="img-fluid img-thumbnail" src={item.image} alt=""/>
-                        </span>
-                    </div>
-                )
-            })
+        span.onclick = function() {
+            modal.style.display = "none";
         }
-    </div>
 
-</div>
-)
+        const imgs = document.querySelectorAll('.imgs img');
+
+        // add onClick on all images
+        imgs.forEach(img => img.addEventListener('click', this.zoom))
+
+    }
+
+    zoom = (event) => {
+        
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var img = document.getElementById(event.target.id);
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+
+
+        modal.style.display = "block";
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+        
+    }
+    render(){
+        return (
+            <div className="container" id='gallery'>
+
+            <h1 className="font-weight-light text-center mt-4 mb-0">CodeImpact Moments</h1>
+
+            <hr className="mt-2 mb-5"/>
+
+            <div className="row text-center text-lg-left imgs">
+                {
+                    galleryData.map((item, index) => {
+                        return (
+                            <div className="col-lg-3 col-md-4 col-6" key={index}>
+                                <span className="d-block mb-4 h-100">
+                                    <img className="img-fluid img-thumbnail myImg" src={item.image} alt="" id={index.toString()}/>
+                                </span>
+                            </div>
+                        )
+                    })
+                }
+                </div>
+                <div id="myModal" className="modal">
+
+                    <span className="close">&times;</span>
+
+                    <img className="modal-content" id="img01" alt=""/>
+
+                    <div id="caption"></div>
+                </div>
+        </div>
+        )
+    }
+}
 
 export default Gallery;
--- src/components/gallery/galleryData.js
+++ src/components/gallery/galleryData.js
@@ -1,30 +1,39 @@
 const galleryData = [
     {
-        image: "https://lh3.googleusercontent.com/EdsTvB4KUOWuogIswmAKwe_B0yrnJZFr-Dqfnpj6zxPbXp8FjEd5csYKk4QLSw1R7ons0pQXjEtvSfmWceH2Y2leWUVpzxlpsOpk_ZJklC3of0nQwLbphXU0m-ZaUq75G6OmYHEDjw=w2400"
+        image: "https://lh3.googleusercontent.com/EdsTvB4KUOWuogIswmAKwe_B0yrnJZFr-Dqfnpj6zxPbXp8FjEd5csYKk4QLSw1R7ons0pQXjEtvSfmWceH2Y2leWUVpzxlpsOpk_ZJklC3of0nQwLbphXU0m-ZaUq75G6OmYHEDjw=w2400",
+        id: "1"
     },
     {
-        image: "https://lh3.googleusercontent.com/HV9lRxpb7S-ggExQ7o-6mVGoqJSzXtjn63ZYz56PHJwPh5NybHyfMBeJ0jeq9twAl1n-yLc7cgWsABviGEcEFE6xfK2Qn8Ud6c_oCNinl0iKOoyI2kFrkT_B-tEniyf5rWQJR52TMQ=w2400"
+        image: "https://lh3.googleusercontent.com/HV9lRxpb7S-ggExQ7o-6mVGoqJSzXtjn63ZYz56PHJwPh5NybHyfMBeJ0jeq9twAl1n-yLc7cgWsABviGEcEFE6xfK2Qn8Ud6c_oCNinl0iKOoyI2kFrkT_B-tEniyf5rWQJR52TMQ=w2400",
+        id: "2"
     },
     {
-        image: "https://lh3.googleusercontent.com/C3piSUoOZbP46yiSMQxTXXNdPZWw4AZWucyv5_50V2SaV2RBOP2k1Aofhw44U1BUCVKw7TUWdbjWAbhpMWGRUWTNfgAAX-u5BL9pI06zIEu6SrDOhQxww6s55pAuDuWCFL6wou03qA=w2400"
+        image: "https://lh3.googleusercontent.com/C3piSUoOZbP46yiSMQxTXXNdPZWw4AZWucyv5_50V2SaV2RBOP2k1Aofhw44U1BUCVKw7TUWdbjWAbhpMWGRUWTNfgAAX-u5BL9pI06zIEu6SrDOhQxww6s55pAuDuWCFL6wou03qA=w2400",
+        id: "3"
     },
     {
-        image: "https://lh3.googleusercontent.com/p-smPHjdbmlVxB4apiks12WRkpIfXzfWCh_40k-NNIS2GDwaI7Aq9xG_MLd_8qwW06CS3YnKBB9i1nZw2xBbB0nGctCaoskwiTBkEPyrZhMPT2XWUduktT05bzITFIGhfyN1AGfrww=w2400"
+        image: "https://lh3.googleusercontent.com/p-smPHjdbmlVxB4apiks12WRkpIfXzfWCh_40k-NNIS2GDwaI7Aq9xG_MLd_8qwW06CS3YnKBB9i1nZw2xBbB0nGctCaoskwiTBkEPyrZhMPT2XWUduktT05bzITFIGhfyN1AGfrww=w2400",
+        id: "4"
     },
     {
-        image: "https://lh3.googleusercontent.com/esL4o7GtvaZSsR6zTz9kVYhsunlRxXXO6DMO0kQ4GFY6v92rSvlbYMK9m6xltPUuMmZNgCqrJ1rIuYVOZUPrxekkUDgRWglurjqJ-ZsKyltit7ogEMuKVw1y28ppnmHk6NujxBaHRw=w2400"
+        image: "https://lh3.googleusercontent.com/esL4o7GtvaZSsR6zTz9kVYhsunlRxXXO6DMO0kQ4GFY6v92rSvlbYMK9m6xltPUuMmZNgCqrJ1rIuYVOZUPrxekkUDgRWglurjqJ-ZsKyltit7ogEMuKVw1y28ppnmHk6NujxBaHRw=w2400",
+        id: "5"
     }
     ,{
-        image: "https://lh3.googleusercontent.com/Gf0FolDxcTBtonC5UKBU5HPbUmYqVXmUpBBYGz_BQTI3KUkxkvj75PLGpuqgL0R4yjui1LAOcZKD65asGwCQRNXe_hON5GYKTGOelKcHkNwiq1RlVFrZQSE8bXjQVXOu4w_BM--HXg=w2400"
+        image: "https://lh3.googleusercontent.com/Gf0FolDxcTBtonC5UKBU5HPbUmYqVXmUpBBYGz_BQTI3KUkxkvj75PLGpuqgL0R4yjui1LAOcZKD65asGwCQRNXe_hON5GYKTGOelKcHkNwiq1RlVFrZQSE8bXjQVXOu4w_BM--HXg=w2400",
+        id: "6"
     },
     {
-        image: "https://lh3.googleusercontent.com/TCmJ7jRIlVH2vN3weVwYq2fbiTSZHxLAyzNsLnj-5HCfIzqwARcQdMoQFc3wLlx8JH1WPcYZsNuDI2xtIbSnccdg0yheMbVW0cr9zaNeJ-Tw3RtNb7vUYy-4LMyG46sff5TjFZxGjA=w2400"
+        image: "https://lh3.googleusercontent.com/TCmJ7jRIlVH2vN3weVwYq2fbiTSZHxLAyzNsLnj-5HCfIzqwARcQdMoQFc3wLlx8JH1WPcYZsNuDI2xtIbSnccdg0yheMbVW0cr9zaNeJ-Tw3RtNb7vUYy-4LMyG46sff5TjFZxGjA=w2400",
+        id: "7"
     },
     {
-        image: "https://lh3.googleusercontent.com/QIxf9jcFKYuFGn_27faXbOsJJHE501_NUxcWkTAgvBf-LfGzIi2S2tbRXUGYc0jYgvhCh1o_oSoQWwMVysnR1-OnBeOipESln5icKA1cAfPaIkt7cs1E_lAfzezNJZmD2wBjNO_ODA=w2400"
+        image: "https://lh3.googleusercontent.com/QIxf9jcFKYuFGn_27faXbOsJJHE501_NUxcWkTAgvBf-LfGzIi2S2tbRXUGYc0jYgvhCh1o_oSoQWwMVysnR1-OnBeOipESln5icKA1cAfPaIkt7cs1E_lAfzezNJZmD2wBjNO_ODA=w2400",
+        id: "8"
     }, 
     {
-        image: "https://lh3.googleusercontent.com/x_tnHs9ThrgYoWBnfX5SRP8RUC7GGhwsNtv1sncuqwGGeeUXy4WVL6GtNRyFiTCvYcP0CgJJgmP19TYuXAhoqhWYtnqrr4RApeSsim9Ii3wjZbNwvM_7J0zK70sftHBvNk7BsoDMeg=w2400"
+        image: "https://lh3.googleusercontent.com/x_tnHs9ThrgYoWBnfX5SRP8RUC7GGhwsNtv1sncuqwGGeeUXy4WVL6GtNRyFiTCvYcP0CgJJgmP19TYuXAhoqhWYtnqrr4RApeSsim9Ii3wjZbNwvM_7J0zK70sftHBvNk7BsoDMeg=w2400",
+        id: "9"
     }
 ]
 
